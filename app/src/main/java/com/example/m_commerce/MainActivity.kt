d92a0b6ec2e2b8e8f8b6f30524e73907bbdccb0d
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-06-15 17:28:08 +0300

    added retrofit

--- app/src/main/java/com/example/m_commerce/MainActivity.kt
+++ app/src/main/java/com/example/m_commerce/MainActivity.kt
@@ -11,8 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.apollographql.apollo.ApolloClient
-import com.example.m_commerce.ui.theme.MCommerceTheme
+import com.example.m_commerce.config.theme.MCommerceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/m_commerce/config/theme/Type.kt
+++ app/src/main/java/com/example/m_commerce/config/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.m_commerce.ui.theme
+package com.example.m_commerce.config.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
