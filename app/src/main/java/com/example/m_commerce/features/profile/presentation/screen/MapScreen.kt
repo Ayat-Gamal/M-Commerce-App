a772ce6f83b5c7abc54c321a4b14209a991c57c1
Mohamed taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2025-06-21 10:45:50 +0300

    feat: Add mapScreen

---
+++ app/src/main/java/com/example/m_commerce/features/profile/presentation/screen/MapScreen.kt
@@ -0,0 +1,182 @@
+package com.example.m_commerce.features.profile.presentation.screen
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavHostController
+import com.example.m_commerce.config.routes.AppRoutes
+import com.example.m_commerce.config.theme.Teal
+import com.example.m_commerce.core.shared.components.CustomButton
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun MapScreenUi(navController: NavHostController) {
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    var currentLocation by remember { mutableStateOf<LatLng?>(null) }
+    var hasLocationPermission by remember { mutableStateOf(false) }
+    val markerState = remember { MarkerState() }
+
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    val locationPermissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasLocationPermission = isGranted
+        if (isGranted) {
+            getCurrentLocation(fusedLocationClient, context) { location ->
+                currentLocation = location
+                markerState.position = location
+            }
+        } else {
+            Toast.makeText(context, "Location permission denied", Toast.LENGTH_SHORT).show()
+            currentLocation = LatLng(26.8206, 30.8025)
+            markerState.position = currentLocation!!
+        }
+    }
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(
+            currentLocation ?: LatLng(26.8206, 30.8025),
+            15f
+        )
+    }
+
+    LaunchedEffect(Unit) {
+        val permissionCheckResult = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        hasLocationPermission = permissionCheckResult == PackageManager.PERMISSION_GRANTED
+
+        if (hasLocationPermission) {
+            getCurrentLocation(fusedLocationClient, context) { location ->
+                currentLocation = location
+                markerState.position = location
+            }
+        } else {
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    LaunchedEffect(currentLocation) {
+        currentLocation?.let {
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 15f)
+        }
+    }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(
+                snackbarHostState,
+                modifier = Modifier.padding(bottom = 100.dp)
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+                onMapClick = { location ->
+                    markerState.position = location
+                    currentLocation = location
+                }
+            ) {
+                currentLocation?.let { location ->
+                    Marker(
+                        state = markerState,
+                        title = "Selected Location",
+                        snippet = "${location.latitude}, ${location.longitude}",
+                        draggable = true,
+                    )
+                }
+            }
+
+            CustomButton(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(bottom = 40.dp)
+                    .width(177.dp)
+                    .align(
+                        Alignment.BottomCenter
+                    ),
+                height = 50,
+                fontSize = 16,
+                text = "Select Location",
+                backgroundColor = Teal,
+                onClick = {
+                    currentLocation?.let {
+                        print("Location selected: ${it.latitude}, ${it.longitude}")
+                        navController.navigate(
+                            AppRoutes.AddAddressScreen(
+                                lat = it.latitude,
+                                lng = it.longitude
+                            )
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+private fun getCurrentLocation(
+    fusedLocationClient: FusedLocationProviderClient,
+    context: Context,
+    onLocationReceived: (LatLng) -> Unit
+) {
+    try {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    location?.let {
+                        onLocationReceived(LatLng(it.latitude, it.longitude))
+                    }
+                }
+                .addOnFailureListener { e ->
+                    onLocationReceived(LatLng(26.8206, 30.8025))
+                }
+        }
+    } catch (e: SecurityException) {
+    }
+}
