175610ff5633aa32f12809d5fbd6eabc19e7a714
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-06-22 23:55:23 +0300

    removed email stuff

--- app/src/main/java/com/example/m_commerce/features/orders/data/datasources/remote/OrderEmailService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.m_commerce.features.orders.data.datasources.remote
-
-import com.example.m_commerce.features.orders.data.model.EmailRequest
-
-interface OrderEmailService {
-    suspend fun sendEmail(emailRequest: EmailRequest)
-}
