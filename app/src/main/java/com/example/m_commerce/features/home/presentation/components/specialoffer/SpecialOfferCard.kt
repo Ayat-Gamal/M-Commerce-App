828da4ef584d82ac80b9516db7edd10416d5d2ef
Mohamed taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2025-06-26 07:47:56 +0300

    feat : update card home screen

--- app/src/main/java/com/example/m_commerce/features/home/presentation/components/specialoffer/SpecialOfferCard.kt
+++ app/src/main/java/com/example/m_commerce/features/home/presentation/components/specialoffer/SpecialOfferCard.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,16 +30,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.m_commerce.config.theme.LightTeal
+import coil3.compose.AsyncImage
+import com.example.m_commerce.config.theme.Black
 import com.example.m_commerce.config.theme.Teal
 import com.example.m_commerce.config.theme.TextBackground
 import com.example.m_commerce.config.theme.White
@@ -56,11 +56,18 @@ fun SpecialOfferCard(
 ) {
     val pagerState = rememberPagerState(initialPage = 0, pageCount = { couponCodes.size })
     val clipboardManager: ClipboardManager = LocalClipboardManager.current
-    val context = LocalContext.current
+
+    val images = listOf(
+        "https://i.pinimg.com/736x/68/83/df/6883dfb9c2db84767bbf0a4c224ce3d0.jpg",
+        "https://i.pinimg.com/736x/d6/ef/49/d6ef490d4caccc2d0bd3904991be4fa6.jpg",
+        "https://i.pinimg.com/736x/48/d8/db/48d8db9ce3074525c652fe725813e6ed.jpg"
+    ,"https://i.pinimg.com/736x/c5/16/93/c51693e58bac33aaadec080b262831a0.jpg"
+    )
+
     LaunchedEffect(Unit) {
         while (isActive) {
             delay(4000)
-            if (couponCodes.size > 0) {
+            if (couponCodes.isNotEmpty()) {
                 val nextPage = (pagerState.currentPage + 1) % couponCodes.size
                 pagerState.animateScrollToPage(nextPage)
             }
@@ -68,107 +75,87 @@ fun SpecialOfferCard(
     }
 
     Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 16.dp),
+        modifier = modifier.fillMaxWidth().padding(vertical = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         HorizontalPager(
             state = pagerState,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(160.dp)
+            modifier = Modifier.fillMaxWidth().height(180.dp)
         ) { page ->
             val coupon = couponCodes[page]
+            val imageUrl = images[page % images.size]
 
             Card(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
-                    .fillMaxSize()
-                    ,
+                    .fillMaxSize(),
                 shape = RoundedCornerShape(16.dp),
                 elevation = CardDefaults.cardElevation(8.dp),
-                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+                colors = CardDefaults.cardColors(containerColor = White)
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                            brush = Brush.horizontalGradient(
-                                colors = listOf(Teal, LightTeal)
-                            ),
-                            shape = RoundedCornerShape(16.dp)
-                        )
-                        .padding(16.dp)
+                Row(
+                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalAlignment = Alignment.CenterVertically
+                    Column(
+                        modifier = Modifier.weight(1f)
                     ) {
-                        Column(
-                            modifier = Modifier.weight(1f)
+                        Text("Offers and Discounts", fontSize = 10.sp, color = Black)
+                        Text(
+                            text = "Get Special Offer",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Black
+                        )
+                        Text(
+                            text = "Up to ${coupon.summary}",
+                            fontSize = 14.sp,
+                            color = Teal
+                        )
+                        Spacer(modifier = Modifier.height(12.dp))
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(50))
+                                .combinedClickable(
+                                    onClick = {
+                                        clipboardManager.setText(AnnotatedString(coupon.code))
+                                        CoroutineScope(Dispatchers.Main).launch {
+                                            snackBarHostState.showSnackbar("Copied: ${coupon.code}")
+                                        }
+                                    },
+                                    onLongClick = {}
+                                )
+                                .background(Teal)
+                                .padding(horizontal = 12.dp, vertical = 6.dp)
                         ) {
-                            Text(
-                                text = "get Gifts",
-                                fontSize = 8.sp,
-                                color = White
+                            Icon(
+                                imageVector = Icons.Default.ContentCopy,
+                                contentDescription = "Copy",
+                                tint = White,
+                                modifier = Modifier.size(16.dp)
                             )
+                            Spacer(modifier = Modifier.width(8.dp))
                             Text(
-                                text = coupon.summary,
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = White
+                                text = coupon.code,
+                                fontSize = 14.sp,
+                                color = White,
+                                fontWeight = FontWeight.Bold
                             )
-                            Text(
-                                text = "Package discount coupon",
-                                fontSize = 8.sp,
-                                color = White
-                            )
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier
-                                    .clip(RoundedCornerShape(50))
-                                    .combinedClickable(
-                                        onClick = {
-                                            clipboardManager.setText(AnnotatedString(coupon.code))
-                                           // Toast.makeText(context, "Copied: ${coupon.code}", Toast.LENGTH_SHORT).show()
-                                            CoroutineScope(Dispatchers.Main).launch {
-                                                snackBarHostState.showSnackbar("Copied: ${coupon.code}")
-                                            }
-                                                  },
-                                        onLongClick = {}
-                                    )
-                                    .background(White)
-                                    .padding(horizontal = 12.dp, vertical = 6.dp)
-
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.ContentCopy,
-                                    contentDescription = "Copy",
-                                    tint = Teal,
-                                    modifier = Modifier.size(16.dp)
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = coupon.code,
-                                    fontSize = 14.sp,
-                                    color = Teal,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
                         }
-
-//                        Icon(
-//                            painter = painterResource(id = R.drawable.coupon),
-//                            contentDescription = null,
-//                            tint = Color.White.copy(alpha = 0.3f),
-//                            modifier = Modifier
-//                                .fillMaxHeight()
-//                                .aspectRatio(1f)
-//                                .padding(end = 8.dp)
-//                        )
                     }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    AsyncImage(
+                        model = imageUrl,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(12.dp))
+                    )
                 }
             }
         }
@@ -184,16 +171,14 @@ fun SpecialOfferCard(
                 Box(
                     modifier = Modifier
                         .padding(horizontal = 4.dp)
-                        .height(8.dp)
-                        .width(8.dp)
-                        .background(
-                            color = if (isSelected) Teal else TextBackground,
-                            shape = CircleShape
-                        )
+                        .size(8.dp)
+                        .clip(CircleShape)
+                        .background(if (isSelected) Teal else TextBackground)
                 )
             }
         }
     }
 }
+
 
 
